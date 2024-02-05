1ddca97695aa6da2383d321d81d80704d8bd9670
Waseem Ahmad <ahmad.w90@gmail.com>
2024-02-05 15:42:47 -0800

    Add display style selector dummy UI

---
+++ app/src/main/java/com/example/android/photoviewer/ui/model/DisplayStyle.kt
@@ -0,0 +1,6 @@
+package com.example.android.photoviewer.ui.model
+
+enum class DisplayStyle {
+    Card,
+    Grid
+}
--- app/src/main/java/com/example/android/photoviewer/ui/photoslist/PhotosListScreen.kt
+++ app/src/main/java/com/example/android/photoviewer/ui/photoslist/PhotosListScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,6 +25,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -39,6 +44,7 @@ import com.example.android.photoviewer.ui.common.ErrorMessage
 import com.example.android.photoviewer.ui.common.LoadingNextPageItem
 import com.example.android.photoviewer.ui.common.PageLoader
 import com.example.android.photoviewer.ui.main.MainViewModel
+import com.example.android.photoviewer.ui.model.DisplayStyle
 import com.example.android.photoviewer.ui.theme.AppTheme
 
 
@@ -76,6 +82,7 @@ fun PhotosListScreen(
                     textAlign = TextAlign.Center)
 
                 ThemeSwitcher(mainViewModel = mainViewModel)
+                DisplayStyleSelector(onStyleChanged = {})
             }
         }
     ) {
@@ -103,6 +110,39 @@ fun ThemeSwitcher(mainViewModel: MainViewModel) {
             modifier = Modifier.size(25.dp)
         )
     }
+}
+
+@Composable
+fun DisplayStyleSelector(onStyleChanged: (DisplayStyle) -> Unit) {
+    val selectedStyle: DisplayStyle = DisplayStyle.Card
+    var expanded by remember { mutableStateOf(false) }
+    IconButton(onClick = { expanded = true }) { 
+        Icon(
+            painter = painterResource(id = R.drawable.ic_vertical_menu), 
+            contentDescription = stringResource(id = R.string.select_display_style),
+            tint = MaterialTheme.colorScheme.onPrimary)
+        DropdownMenu(
+            expanded = expanded, 
+            onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(
+                text = { Text(text = stringResource(id = R.string.display_style_card)) }, 
+                onClick = { onStyleChanged(DisplayStyle.Card) },
+                trailingIcon = {  DisplayStyleDropdownMenuItemTrailingIcon(DisplayStyle.Card, selectedStyle)})
+            DropdownMenuItem(
+                text = { Text(text = stringResource(id = R.string.display_style_grid)) }, 
+                onClick = { onStyleChanged(DisplayStyle.Grid) },
+                trailingIcon = { DisplayStyleDropdownMenuItemTrailingIcon(DisplayStyle.Grid, selectedStyle) })
+        }
+    }
+}
+
+@Composable
+fun DisplayStyleDropdownMenuItemTrailingIcon(styleItem: DisplayStyle, selectedStyle: DisplayStyle) {
+    if (styleItem != selectedStyle) {
+        return
+    }
+    Icon(painter = painterResource(id = R.drawable.baseline_check),
+                        contentDescription = null)
 }
 
 @Composable
