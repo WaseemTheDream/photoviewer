cbdf1aa806a8c50f19c786aaff67ce9bb59c57ad
Waseem Ahmad <ahmad.w90@gmail.com>
2024-02-05 14:59:48 -0800

    Update color scheme

--- app/src/main/java/com/example/android/photoviewer/ui/theme/Color.kt
+++ app/src/main/java/com/example/android/photoviewer/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.example.android.photoviewer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val colorPrimary = Color(0xFFB02E2E)
+val colorSecondary = Color(0xFFB02E2E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val colorBlack = Color(0xFF0C0C0C)
+val colorBlack100 = Color(0xFF17202A)
+
+val colorWhite = Color(0xFFFFFFFF)
+val colorWhite100 = Color(0xFFDAB2B2)
+
+val colorGrayDark = Color(0xFF0E141C)
+val colorGrayLight = Color(0xFFE7E7E7)
+
+
+val colorRed = Color(0xFFFF0000)
